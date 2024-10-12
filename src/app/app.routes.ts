e8570629987b5df9c047b85231433499c6e0df47
Abdullahnabil22 <abdullahn.work@gmail.com>
2024-10-12 16:39:50 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { DashboardComponent } from './Components/dashboard/dashboard.component';
 import { ApprovedListComponent } from './Components/approved-list/approved-list.component';
 import { ApartmentListComponent } from './Components/apartment-list/apartment-list.component';
 import { VisitorChartComponent } from './Components/visitors-chart/visitors-chart.component';
-import { UsersListControlComponent } from './Components/users-list-control/users-list-control.component';
 import { LoginComponent } from './Components/login/login.component';
 import { EarningChartComponent } from './Components/earning-chart/earning-chart.component';
 import { DetailsComponent } from './Components/details/details.component';
 import { userauthGuard } from './Guards/guards.service';
 import { MainlayoutComponent } from './Components/mainlayout/mainlayout.component';
+import { UsersListComponent } from './Components/users-list-control/users-list-control.component';
 export const routes: Routes = [
   {
     path: '',
@@ -19,7 +19,7 @@ export const routes: Routes = [
       { path: 'approved-list', component: ApprovedListComponent },
       { path: 'apartment-list', component: ApartmentListComponent },
       { path: 'earning-chart', component: EarningChartComponent },
-      { path: 'users-list-control', component: UsersListControlComponent },
+      { path: 'users-list-control', component: UsersListComponent },
       { path: 'visitors-chart', component: VisitorChartComponent },
       { path: 'approved-list/details/:id', component: DetailsComponent },
     ],
